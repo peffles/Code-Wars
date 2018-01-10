4ee14fb6397a864427b76061748bcbce5153dd9e
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2018-01-09 22:33:28 -0800

    F-U-N-C-T-I-O-N function

--- name-abbreviation/solution.js
+++ name-abbreviation/solution.js
@@ -1,5 +1,5 @@
-funtion abbrevName(name) {
-    var seperate = name.split(' ').map(function(name){return name[0]}).join('.');
-    var caps = seperate.toUpperCase();
-    return caps;
+function abbrevName(name) {
+  var seperate = name.split(' ').map(function(name){return name[0]}).join('.');
+  var caps = seperate.toUpperCase();
+  return caps;
 }
